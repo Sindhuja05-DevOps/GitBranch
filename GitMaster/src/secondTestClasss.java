0a1d0874d49a9eaf0fc804a774f1f342699c1efc
sindhuja <sindhujaramesh10@gmail.com>
2020-06-30 10:39:35 -0500

    commit from tester

---
+++ GitMaster/src/secondTestClasss.java
@@ -0,0 +1,10 @@
+
+public class secondTestClasss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the code from tester");
+
+	}
+
+}
